68715f0dd457de8c21079446bc4e9f84639532f3
antonio132312 <fraaortiz1@misena.edu.co>
2023-07-19 17:59:12 -0500

    avance navbar

--- Frontend/src/Components/Nabvar.jsx
+++ Frontend/src/Components/Nabvar.jsx
@@ -2,26 +2,32 @@ import React from "react";
 import LogoB from "../Img/Logo.png"
 import Logoin from "../Img/ingresar.png"
 import Logomeg from "../Img/megusta.png"
+import Carrito from "../Img/Carrito.png"
+import Lupa from "../Img/lupa.png"
 
 export default function Nabvar() {
     return (
         <nav className="navbar">
         <img src={LogoB} id="logo" />
-            <input type="checkbox" id="toggler"></input>
-            <label for="toggler"><i><img id="imgtoggler" src="./Img/NavBar/toggler.png" /></i></label>
+        <div className="Contener-busqueda">
+        <div className="Barra-buscas">
+        <input type="search" id="Buscar" placeholder=" Busqueda"  onchange="error(event)"/>
+        <button id="btn_buscar" onclick="ejecutar()"> <img src={Lupa}></img></button>
+    </div>
+    </div>
             <div className="menu">
                 <ul className="list">
-                    <li>
+                    <li className="aten">
                         <a>Atencion al cliente</a>
                     </li>
+                    <li className="ini">
                     <img src={Logoin} id="logoin"  />
-                    <img src={Logomeg} id="logomeg"  />
-                  
-                    <li className="listacerca">
-                        <a href="./Pages/acerca_de.html"> ACERCA DE</a>
                     </li>
-                    <li>
-                        <a href="./Pages/Busqueda.html"><button class="btnlupa"><img id="lupa" src="./Img/NavBar/lupa.png" /></button></a>
+                    <li className="meg">
+                    <img src={Logomeg} id="logomeg"  />
+                    </li>
+                    <li className="car">
+                    <img src={Carrito} id="Carrito"  />
                     </li>
                 </ul>
             </div>
