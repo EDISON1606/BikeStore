f7ddd4b51e96474dbe9b9b0bcd3636f19b92f198
Edison Cuaran <eycuaran2003@gmail.com>
2023-07-30 21:37:28 -0500

    Avances

--- Frontend/src/Components/Nabvar.jsx
+++ Frontend/src/Components/Nabvar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import LogoB from "../Img/Logo.png";
 import Logoin from "../Img/ingresar.png";
 import Logomeg from "../Img/megusta.png";
@@ -14,40 +14,58 @@ export default function Nabvar() {
     const redireccionar = ()=>{
 
         navigate("/Inicio");
+    const [clicked, setClicked] = useState(False)
     }
 
     
 
     return (
         <nav className="navbar">
-        <img src={LogoB} id="logo" />
+        <div className="logob"><img src={LogoB} id="logo" /></div>
         <div className="Contener-busqueda">
         <div className="Barra-buscas">
         <input type="search" id="Buscar" placeholder=" Busqueda"  onchange="error(event)"/>
-        <button id="btn_buscar" onclick="ejecutar()"> <img src={Lupa}></img></button>
+        <button id="btn_buscar" onclick="ejecutar()"> <img src={Lupa} className="lupa"></img></button>
     </div>
     </div>
-    <label for="toggler"><i><img id="imgtoggler" src={Menu} /></i></label>
+    <div className="icon3">
+    <div className="ini2">
+                    <abbr title="Opciones"><li><a href="#"><img src={Logoin} alt="inicio/registro" id="logoin"/></a></li>
+                    <select id="selector" onChange={redireccionar}>
+                        <option></option>
+                        <option>Inicio   </option> 
+                        <option>Registro</option>
+                        </select></abbr>
+                      </div>
+                      <div className="car">
+                    <abbr title="Carrito2"><a href="/"><img src={Carrito} id="Carrito"  /></a></abbr>
+                    </div>
+    <div id="nav-icon3">
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+</div>
+</div>
             <div className="menu">
                 <ul className="list">
                     <li className="aten">
-                        <a>Atencion al cliente</a>
+                        <a href="/">Atencion al cliente</a>
                     </li>
                     <div className="ini">
-                    <li><a href="#"><img src={Logoin} alt="inicio/registro" id="logoin"/></a></li>
-                    <li><select id="" onChange={redireccionar}>
-                        <option value="0"></option>
-                        <option className="inicio">Inicio</option>
-                        <option value="2">Registro</option>
-                        </select></li>
+                    <abbr title="Opciones"><li><a href="#"><img src={Logoin} alt="inicio/registro" id="logoin"/></a></li>
+                    <select id="selector" onChange={redireccionar}>
+                        <option></option>
+                        <option>Inicio   </option> 
+                        <option>Registro</option>
+                        </select></abbr>
                       </div>
                     <li className="meg">
-                    <img src={Logomeg} id="logomeg"  />
+                    <abbr title="Favoritos"><a href="/"><img src={Logomeg} id="logomeg"  /></a></abbr>
                     </li>
                     <li className="car">
-                    <a href="/Inicio" ><img src={Carrito} id="Carrito"  /></a>
+                    <abbr title="Carrito"><a href="/"><img src={Carrito} id="Carrito"  /></a></abbr>
                     </li>
-                  
                 </ul>
             </div>
     </nav>
