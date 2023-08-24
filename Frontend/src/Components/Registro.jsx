3677226582ef65a0ffac0e627c3fc068ee749612
antonio132312 <fraaortiz1@misena.edu.co>
2023-08-24 17:44:20 -0500

    avances

--- Frontend/src/Components/Registro.jsx
+++ Frontend/src/Components/Registro.jsx
@@ -1,4 +1,25 @@
 import React from 'react'
+/*
+async function onSubmit(DataRegister){
+  setIsLoader(true);
+  console.log(DataRegister);
+}
+*/
+  try {
+    const response = await fetch('http://localhost:3060/registrar',{
+      method: 'POST',
+      headers: {
+        'Content--Type': 'application/json'
+      },
+      body: JSON.stringify(DataRegister)
+    });
+  }
+  finally{
+    setIsLoader(false);
+  }
+
+
+
 
 const Registro = () => {
   return (
@@ -6,23 +27,23 @@ const Registro = () => {
       <div className="formulario2">
       <h1>Registrate en Bike Store!</h1>
       <form method="post">
-        <div className="username2">
+        <div className="username2" name="nombres" value={1}>
           <input type="text" placeholder=' ' required/> 
             <label>Nombres</label>
         </div>
-        <div className="username2">
+        <div className="username2" name="apellidos" value={2}>
           <input type="text" placeholder=' ' required/>
             <label>Apellidos</label>
         </div>
-        <div className="username2">
+        <div className="username2" name="correo" value={3}>
           <input type="Email" placeholder=' ' required/>
             <label>Correo electronico</label>
         </div>
-        <div className="username2">
+        <div className="username2" name="contraseña" value={4}>
           <input type="Password" placeholder=' ' required/>
             <label>Contraseña</label>
         </div>
-        <div className="username2">
+        <div className="username2" value={5}>
           <input type="Password" placeholder=' ' required/>
             <label>Confirmar contraseña</label>
         </div>
@@ -33,8 +54,7 @@ const Registro = () => {
       </form>
     </div>
     </div>
-  )
+  ) 
 }
-
 
 export default  Registro
