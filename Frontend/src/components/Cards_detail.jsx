b74ce93d8491d3cf37de9f5b4ac42f4408ff1b59
jeni84 <willischurro16@gmail.com>
2023-09-01 17:38:49 -0500

    avance detalle

--- Frontend/src/components/Cards_detail.jsx
+++ Frontend/src/components/Cards_detail.jsx
@@ -28,12 +28,40 @@ export default function Cards_detail (){
                     </li>
                     <li>
                         <h6>Color:</h6>
-                        
+                        <select className="talla" >
+                            <option className="opcion_uno" value="" ></option>
+                            <option className="A" value="" disabled="true"></option>
+                            <option className="opcion_dos" value="">Rojo</option>
+                            <option className="A" value="" disabled="true"></option>
+                            <option className="opcion_tres" value="">Blanco</option>
+                            <option className="A" value="" disabled="true"></option>
+                            <option className="opcion_cuatro" value="">Negro</option>
+                            <option className="A" value="" disabled="true"></option>
+                            <option className="opcion_cinco" value="">Azul</option>
+                            <option className="A" value="" disabled="true"></option>
+                        </select>
                     </li>
                     </ul>
-                    <button className=''></button>
+                    <div className='colores'>
+                    <button className='color-1'></button>
+                        <button className='color-2'></button>
+                        <button className='color-3'></button>
+                        <button className='color-4'></button>
+                        </div>
+                    <div className='contador'>
+                    <button className="btn-menos">-</button>
+                    
+                    <div className='btn-numer'>
+                        <input type="text" value="1" className="numer-produ" disabled="true"></input>
+                    </div>
+                 
+                    <button className="btn-más">+</button>
+                    <p>Máximo 20 Unidades</p>
+                    </div>
+     
                 </div>      
                 <button className='btn2'>Agregar Carrito</button>                              
         </div>
+        
     );
 };
